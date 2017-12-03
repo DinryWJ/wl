f05954caefedb762cbac3c4507db779d8e09061d
Jiajie Wu <827121759@qq.com>
2017-12-03 16:41:17 +0800

    0.13
    删除了一些无用的test class

--- src/test/java/com/zust/dao/BaseDaoTest.java
+++
@@ -1,9 +0,0 @@
-package com.zust.dao;
-
-import org.unitils.UnitilsJUnit4;
-import org.unitils.spring.annotation.SpringApplicationContext;
-
-@SpringApplicationContext( {"classpath:/applicationContext.xml" })
-public class BaseDaoTest extends UnitilsJUnit4{
-	
-}
