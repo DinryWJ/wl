8f85f336685dd4fa992c0f5f598ecb5be4a4fffe
Jiajie Wu <827121759@qq.com>
2017-12-04 15:08:23 +0800

    0.22
    增加了Spring事务管理

--- src/main/java/com/zust/dao/UserDAO.java
+++ src/main/java/com/zust/dao/UserDAO.java
@@ -1,5 +1,6 @@
 package com.zust.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.hibernate.Session;
@@ -17,11 +18,28 @@ public class UserDAO{
 	
 	
 	public User getUserById(int id){
-		Session session = sessionFactory.openSession();
+		Session session = sessionFactory.getCurrentSession();
 		String hql="from User where userId=?";
-		
 		User user = (User) session.createQuery(hql).setParameter(0, id).uniqueResult();
 		return user;
 	}
-
+	public void createUser(String password,String name,String address,String phone,boolean gender,int price){
+		User user = new User();
+		user.setAddress(address);
+		user.setCreatetime(new Date());
+		user.setGender(gender);
+		user.setIntegral(0);
+		user.setName(name);
+		user.setPassword(password);
+		user.setPhone(phone);
+		user.setPrice(price);
+		user.setType(false);
+		user.setUpdatetime(new Date());
+		sessionFactory.getCurrentSession().save(user);
+	}
+	public void updateUser(int id,String password,String address,String phone,boolean gender){
+		Session session = sessionFactory.getCurrentSession();
+		session.get(User.class, id);
+		
+	}
 }
--- src/main/java/com/zust/service/UserService.java
+++ src/main/java/com/zust/service/UserService.java
@@ -2,10 +2,11 @@ package com.zust.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.zust.dao.UserDAO;
 import com.zust.entity.User;
-
+@Transactional
 @Service
 public class UserService {
 	@Autowired
